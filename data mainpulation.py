62eda2d064d1f1d650c2e65e05015f681b244765
vishwajeet <vishwajeetdupargude600@gmail.com>
2025-09-10 20:13:01 +0530

    two function

--- data mainpulation.py
+++ data mainpulation.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("C:\\Users\\Vishwajeet\\Downloads\\sql\\SQL - Retail Sales Analysis_utf .csv", encoding="latin1")
 
-print(df)
+print(df.info())
+
+#print(df.head())  #showing data's staring 5 lines
+
+#print(df.tail())  #showing data's ending 5 lines
