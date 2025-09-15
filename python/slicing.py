e18e812133cf40cfe6703971d5469a16cd2b3694
vishwajeet <vishwajeetdupargude600@gmail.com>
2025-09-15 22:27:32 +0530

    slicing the letters

---
+++ python/slicing.py
@@ -0,0 +1,15 @@
+a="vishwajeet"
+print(a[2::5])
+
+
+print(a[::1])#for all letters
+
+print(a[::-1]) #for a reverse string
+
+print(a[-8::-2]) #for step one letter to jump next
+
+print(a[0:9].upper()) #for all letters in capital
+
+print(a[:]) #all letters
+
+print(a[::-1])
